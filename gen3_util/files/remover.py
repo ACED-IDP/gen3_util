fbba6c3c28037c66ac4b56df45bf34333700bb01
Liam Beckman <lbeckman314@gmail.com>
2023-12-11 11:15:10 -0800

    Add response status code

--- gen3_util/files/remover.py
+++ gen3_util/files/remover.py
@@ -4,10 +4,9 @@ from gen3_util.config import Config, gen3_services
 
 def rm(config: Config, object_id: str = None):
     """Remove files."""
-    print_formatted(config, {'msg': 'file removal message goes here'})  # TODO implement
-    file_client, index_client, user, auth = gen3_services(config=config)
+    file_client, _, _, _ = gen3_services(config=config)
     if object_id:
         resp = file_client.delete_file_locations(object_id)
-        return resp
+        return {'response': resp.status_code, 'msg': resp.text}
 
     return None
