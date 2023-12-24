8599b197f73ed86348a89555b6d10fd8507abbb1
Brian Walsh <brian@bwalsh.com>
2023-12-24 06:24:02 -0800

    Move to integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,9 @@
 import os
 
 import pytest
+from gen3.submission import Gen3Submission
+
+from gen3_util.config import ensure_auth
 
 
 @pytest.fixture
@@ -14,10 +17,18 @@ def profile() -> str:
     """gen3-client profile to use for testing."""
     if 'GEN3_UTIL_PROFILE' in os.environ:
         return os.environ['GEN3_UTIL_PROFILE']
-    return "aced-training"
+    print("GEN3_UTIL_PROFILE not set, using profile 'local'")
+    return "local"
 
 
 @pytest.fixture
 def program() -> str:
     """program to use for testing projects i.e. program-projects"""
     return "test"
+
+
+@pytest.fixture
+def submission_client() -> str:
+    """Gen3Submission client"""
+    auth = ensure_auth(profile='development')
+    return Gen3Submission(auth_provider=auth)
