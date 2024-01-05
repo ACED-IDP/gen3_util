358305dbda78d82380a5b0cffb2afada56caa974
Brian Walsh <brian@bwalsh.com>
2024-01-05 15:10:20 -0800

    bump 0.0.14rc1, [g3t,gen3-tracker]

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # package, this name will be registered for you. It will determine how
     # users can install this project, e.g.:
     #
-    # $ pip install hop_etl
+    # $ pip install gen3_util
     #
     # And where it will live on PyPI: https://pypi.org/project/drs_downloader/
     #
@@ -41,7 +41,7 @@ setup(
 
     # Versions should comply with PEP 440:
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.13',  # Required
+    version='0.0.14rc1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -121,7 +121,9 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'gen3_util = gen3_util.cli.cli:cli'
+            'gen3_util = gen3_util.cli.cli:cli',
+            'g3t = gen3_util.cli.cli:cli',
+            'gen3-tracker = gen3_util.cli.cli:cli',
         ],
     },
 
