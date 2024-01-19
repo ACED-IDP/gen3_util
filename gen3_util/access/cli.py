278db13f7b203c34e7ca1b2e0fb50f83ac418f50
Brian Walsh <brian@bwalsh.com>
2024-01-19 09:20:31 -0800

    restores add cmd

--- gen3_util/access/cli.py
+++ gen3_util/access/cli.py
@@ -1,9 +1,11 @@
 import click
 
+from gen3_util.access import create_request
 from gen3_util.access.requestor import ls, cat, update, LogAccess
 from gen3_util.access.submitter import ensure_program_project
 from gen3_util.cli import CLIOutput
 from gen3_util.cli import NaturalOrderGroup
+from gen3_util.common import validate_email
 from gen3_util.config import Config, ensure_auth
 
 
@@ -12,6 +14,37 @@ from gen3_util.config import Config, ensure_auth
 def access_group(config: Config):
     """Manage access requests."""
     pass
+
+
+@access_group.command(name="add")
+@click.argument('user_name')
+@click.option('--resource_path', default=None, required=False, show_default=True,
+              help="Gen3 authz /programs/<program>")
+@click.option('--roles', show_default=True, default=None, help='Add comma-delimited role permissions to the access request, ex: --roles "storage_writer,file_uploader"')
+@click.pass_obj
+def access_touch(config: Config,  resource_path: str, user_name: str, roles: str):
+    """Create a request a specific role.
+
+    \b
+    USER_NAME (str): user's email
+
+    """
+    msgs = validate_email(user_name)
+    assert msgs == [], f"Invalid email address: {user_name} {msgs}"
+
+    assert user_name, "required"
+
+    request = {"username": user_name, "resource_path": resource_path}
+    if roles is not None:
+        roles = list(map(str, roles.split(',')))
+        request.update({"role_ids": roles})
+
+    with CLIOutput(config=config) as output:
+        try:
+            output.update(create_request(config=config, request=request))
+        except AssertionError as e:
+            output.update({'msg': str(e)})
+            output.exit_code = 1
 
 
 @access_group.command(name="sign")
@@ -66,7 +99,11 @@ def sign(config: Config, username: str):
 def access_ls(config: Config, mine: bool, active: bool, username: str):
     """List current user's requests."""
     with CLIOutput(config=config) as output:
-        output.update(ls(config, mine, active, username))
+        msg = 'OK'
+        access = ls(config, mine, active, username)
+        if not access.requests or len(access.requests) == 0:
+            msg = 'No unsigned requests'
+        output.update({'requests': access.requests, 'msg': msg})
 
 
 @access_group.command(name="cat")
