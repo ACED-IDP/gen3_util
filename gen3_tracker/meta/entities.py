7bc7d7bb30cc228e39b38722861fa5a973c5be0f
quinnwai <quinnwai.wong@gmail.com>
2024-09-09 12:07:56 -0700

    add SimplifiedCondition to override codings() method

--- gen3_tracker/meta/entities.py
+++ gen3_tracker/meta/entities.py
@@ -258,6 +258,7 @@ class SimplifiedFHIR(BaseModel):
             elif isinstance(v, list):
                 for elem in v:
                     if isinstance(elem, dict):
+                        # TODO: implement hierarchy of codes rather than just taking last code?
                         for value, source in normalize_coding(elem):
                             if len(v) > 1 and get_nested_value(elem, [source, 0, 'system']):
                                 _codings[elem[source][0]["system"].split("/")[-1]] = value
@@ -392,6 +393,27 @@ class SimplifiedDocumentReference(SimplifiedFHIR):
                         continue
                     _values[k] = v
         return _values
+    
+class SimplifiedCondition(SimplifiedFHIR):
+    @computed_field
+    @property
+    def codings(self) -> dict:
+        # only go through the work if code exists
+        if "code" not in self.resource:
+            return {}
+        
+        # get field name
+        codings_dict = super().codings
+        if "category" in codings_dict:
+            key = codings_dict["category"]
+            del codings_dict["category"]
+        else:
+            key = "code"
+        
+        # TODO: implement hierarchy of codes rather than just taking last code?
+        value, _ = normalize_coding(self.resource["code"])[-1]
+        return {key: value}
+
 
 class SimplifiedResource(object):
     """A simplified FHIR resource, a factory method."""
@@ -405,4 +427,10 @@ class SimplifiedResource(object):
             return SimplifiedObservation(resource=resource)
         if resource_type == "DocumentReference":
             return SimplifiedDocumentReference(resource=resource)
+<<<<<<< HEAD
         return SimplifiedFHIR(resource=resource)
+=======
+        if resource_type == "Condition":
+            return SimplifiedCondition(resource=resource)
+        return SimplifiedFHIR(resource=resource)
+>>>>>>> 1e502e4 (add SimplifiedCondition to override codings() method)
