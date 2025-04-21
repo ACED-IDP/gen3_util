2536bcab1ca820a9e8e5d5be3d79d677d8bcbbc9
Brian Walsh <brian@bwalsh.com>
2025-04-21 16:55:10 -0700

    adds rm tests

---
+++ tests/integration/test_rm_file.py
@@ -0,0 +1,115 @@
+import os
+from pathlib import Path
+from click.testing import CliRunner
+from tests import run
+import pytest
+
+
+def test_rm_uncommitted(runner: CliRunner, project_id, tmpdir) -> None:
+    """Ensure we can remove uncommitted files."""
+    # change to the temporary directory
+    assert tmpdir.chdir()
+    print(Path.cwd())
+
+    _create_project(project_id, runner)
+
+    # rm the second test file before committing
+    run(
+        runner,
+        ["--debug", "rm", str("my-project-data/hello2.txt")],
+    )
+
+    # create the meta file
+    run(
+        runner,
+        ["--debug", "meta", "init"],
+        expected_files=[Path("META/DocumentReference.ndjson")],
+    )
+
+    # commit the changes, delegating to git
+    run(runner, ["--debug", "commit", "-am", "initial commit"])
+
+    # push to the server
+    run(runner, ["--debug", "push"])
+
+    # list the files from indexd
+    run(runner, ["--debug", "ls"], expected_output=["my-project-data/hello.txt"])
+
+    # list the files from indexd, should not include the removed file
+    with pytest.raises(AssertionError):
+        run(runner, ["--debug", "ls"], expected_output=["my-project-data/hello2.txt"])
+
+
+def test_rm_committed(runner: CliRunner, project_id, tmpdir) -> None:
+    """Ensure we can remove uncommitted files."""
+    # change to the temporary directory
+    assert tmpdir.chdir()
+    print(Path.cwd())
+
+    _create_project(project_id, runner)
+
+    # create the meta file
+    run(
+        runner,
+        ["--debug", "meta", "init"],
+        expected_files=[Path("META/DocumentReference.ndjson")],
+    )
+
+    # commit the changes
+    run(runner, ["--debug", "commit", "-am", "initial commit"])
+
+    # rm the second test file after committing
+    run(
+        runner,
+        ["--debug", "rm", str("my-project-data/hello2.txt")],
+    )
+
+    # re-create the meta file
+    run(
+        runner,
+        ["--debug", "meta", "init"],
+        expected_files=[Path("META/DocumentReference.ndjson")],
+    )
+
+    # commit the re-created meta
+    run(runner, ["--debug", "commit", "-am", "updated commit"])
+
+    # push to the server
+    run(runner, ["--debug", "push"])
+
+    # list the files from indexd
+    run(runner, ["--debug", "ls"], expected_output=["my-project-data/hello.txt"])
+
+    # list the files from indexd, should not include the removed file
+    with pytest.raises(AssertionError):
+        run(runner, ["--debug", "ls"], expected_output=["my-project-data/hello2.txt"])
+
+
+def _create_project(project_id, runner, add_files=True, files=("my-project-data/hello.txt", "my-project-data/hello2.txt")) -> list[str]:
+    """Create a project and add files to it."""
+
+    assert os.environ.get(
+        "G3T_PROFILE"
+    ), "G3T_PROFILE environment variable must be set."
+    print(project_id)
+    run(
+        runner,
+        ["--debug", "init", project_id, "--approve"],
+        expected_files=[Path(".g3t"), Path(".git")],
+    )
+
+    for path in files:
+        # create a test file
+        test_file = Path(path)
+        test_file.parent.mkdir(parents=True, exist_ok=True)
+        test_file.write_text("hello\n")
+
+        # add the file
+        if add_files:
+            run(
+                runner,
+                ["--debug", "add", str(test_file)],
+                expected_files=[Path(f"MANIFEST/{path}.dvc")],
+            )
+
+    return files
