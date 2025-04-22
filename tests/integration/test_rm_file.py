859cbc3c89c3e948f71e61b010c9e9385d6ea59b
Brian Walsh <brian@bwalsh.com>
2025-04-22 14:38:23 -0700

    validate_document_in_grip/elastic

--- tests/integration/test_rm_file.py
+++ tests/integration/test_rm_file.py
@@ -1,8 +1,15 @@
 import os
 from pathlib import Path
+
+import yaml
 from click.testing import CliRunner
+
+from gen3_tracker.config import ensure_auth, default
+from gen3_tracker.git import DVC
 from tests import run
 import pytest
+
+from tests.integration import validate_document_in_grip, validate_document_in_elastic
 
 
 def test_rm_uncommitted(runner: CliRunner, project_id, tmpdir) -> None:
@@ -75,7 +82,7 @@ def test_rm_committed(runner: CliRunner, project_id, tmpdir) -> None:
     run(runner, ["--debug", "commit", "-am", "updated commit"])
 
     # push to the server
-    run(runner, ["--debug", "push"])
+    run(runner, ["--debug", "push", "--fhir-server"])
 
     # list the files from indexd
     run(runner, ["--debug", "ls"], expected_output=["my-project-data/hello.txt"])
@@ -83,6 +90,24 @@ def test_rm_committed(runner: CliRunner, project_id, tmpdir) -> None:
     # list the files from indexd, should not include the removed file
     with pytest.raises(AssertionError):
         run(runner, ["--debug", "ls"], expected_output=["my-project-data/hello2.txt"])
+
+    # check the files exist in the graph and flat databases
+    # we will need the object_id of the file to do that
+    # should create a dvc file
+    dvc_path = Path("MANIFEST/my-project-data/hello.txt.dvc")
+    assert dvc_path.exists(), f"{dvc_path} does not exist."
+    with open(dvc_path) as f:
+        yaml_data = yaml.safe_load(f)
+    assert yaml_data
+    dvc = DVC.model_validate(yaml_data)
+    assert dvc, "DVC file not parsed."
+
+    # capture expected object_id
+    dvc.project_id = project_id
+    object_id = dvc.object_id
+    auth = ensure_auth(config=default())
+    validate_document_in_grip(object_id, auth=auth, project_id=project_id)
+    validate_document_in_elastic(object_id, auth=auth)
 
 
 def _create_project(project_id, runner, add_files=True, files=("my-project-data/hello.txt", "my-project-data/hello2.txt")) -> list[str]:
