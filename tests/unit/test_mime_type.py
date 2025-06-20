cabdc7510cffa54d111ba9c96bd4efc0a12c7275
teslajoy <nasim@plenary.org>
2025-06-20 08:20:42 -0700

    pass pytest tests/unit/test_mime_type.py

--- tests/unit/test_mime_type.py
+++ tests/unit/test_mime_type.py
@@ -5,3 +5,5 @@ def test_fastq_mime_type():
     """Ensure we can get the mime type for a fastq file."""
     assert get_mime_type("tests/data/test.fastq") == "text/fastq"
     assert get_mime_type("tests/data/test.fastq.gz") == "text/fastq"
+    assert get_mime_type("tests/data/test.svs") == "image/x-svs"
+
